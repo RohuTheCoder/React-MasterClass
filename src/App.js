8fb154ca36b5b436ac68bd251ebd7db655af164f
rohini.gb <rohugb777999@gmail.com>
2024-02-04 16:47:15 +0530

    Props in React

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome';
 import Hello from './components/Hello'
+import Hello1 from './components/Hello1'
 
 function App() {
   return (
     <div className="App">
-     {/* <Greet /> */}
-     <Welcome /> 
-   
+      <Greet name="Bruce" heroName="Batman">
+      <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman">
+      <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Women"/>
+      <Welcome name="Bruce" heroName="Batman"/>
+      <Welcome name="Clark" heroName="Superman"/>
+      <Welcome name="Diana" heroName="Wonder Women"/>
+     {/*<Welcome /> */}
+     {/* <Hello /> */}
+    {/*<Hello1 />*/}
     </div>
   );
 }
